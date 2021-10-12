160990e06f81805f2d86ab5c85dc0f3e712461d2
goze <goze.qiu@gmail.com>
2021-10-12 21:21:22 +0800

    chore: 合约初始化

--- sanmi/contract/models.py
+++ sanmi/contract/models.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from dataclasses import dataclass
 from datetime import datetime
 from sanmi.database import db
@@ -7,22 +8,39 @@ from sanmi.extensions import bcrypt
 @dataclass
 class Contract(db.Model):
     id: int
+    # 合约编号
     number: str
+    # 客户姓名
     client_name: str
+    # 客户电话
     client_phone_1: str
+    # 客户备用电话
     client_phone_2: str
+    # 客户微信号
+    client_wechat: str
+    # 客户地址
+    client_address: str
+    # 地上楼层
     above_floor: int
+    # 地下楼层
     under_floor: int
+    # 居室数量
     bedroom_num: int
+    # 客厅数量
     living_room_num: int
+    # 卫生间数量
+    rest_room_num: int
+    # 客户来源
     client_source: int
+    # 成交金额
     price: float
+    # 礼品
     gift: int
+    # 工期
     work_time: int
     create_at: datetime
     update_at: datetime
     delete_at: datetime
-    # users: User
 
     id = db.Column(db.Integer, primary_key=True)
     number = db.Column(db.String(80), unique=True, nullable=False)
@@ -32,16 +50,16 @@ class Contract(db.Model):
     client_wechat = db.Column(db.String(50), unique=False, nullable=True)
     client_address = db.Column(db.String, unique=False, nullable=True)
     # 地上楼层
-    above_floor = db.Column(db.Numeric)
+    above_floor = db.Column(db.Integer)
     # 地下楼侧
-    under_floor = db.Column(db.Numeric)
-
-    bedroom_num = db.Column(db.Numeric)
-    living_room_num = db.Column(db.Numeric)
-    client_source = db.Column(db.Numeric)
+    under_floor = db.Column(db.Integer)
+    bedroom_num = db.Column(db.Integer)
+    living_room_num = db.Column(db.Integer)
+    rest_room_num = db.Column(db.Integer)
+    client_source = db.Column(db.Integer)
     price = db.Column(db.Float)
-    gift = db.Column(db.Numeric)
-    work_time = db.Column(db.Numeric)
+    gift = db.Column(db.Integer)
+    work_time = db.Column(db.Integer)
 
     create_at = db.Column(db.DateTime, default=datetime.now)
     update_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
--- sanmi/contract/router.py
+++ sanmi/contract/router.py
@@ -13,7 +13,52 @@ blueprint = Blueprint("contract", __name__, url_prefix="/contract")
 @blueprint.route("/")
 @jwt_required()
 @auth_role(['admin'])
-def all_user():
+def all_contract():
     return jsonify({
-        "data": 1
+        "data": Contract.query.all()
     })
+
+
+@blueprint.route("/", methods=["POST"])
+@jwt_required()
+@auth_role(['admin'])
+def all_user():
+    number = request.json.get('number', None)
+    client_name = request.json.get('client_name', None)
+    client_phone_1 = request.json.get('client_phone_1', None)
+    client_phone_2 = request.json.get('client_phone_2', None)
+    client_wechat = request.json.get('client_wechat', None)
+    client_address = request.json.get('client_address', None)
+    above_floor = request.json.get('above_floor', None)
+    under_floor = request.json.get('under_floor', None)
+    bedroom_num = request.json.get('bedroom_num', None)
+    living_room_num = request.json.get('living_room_num', None)
+    rest_room_num = request.json.get('rest_room_num', None)
+    client_source = request.json.get('client_source', None)
+    price = request.json.get('price', None)
+    gift = request.json.get('gift', None)
+    work_time = request.json.get('work_time', None)
+
+    try:
+        new_item = Contract(number=number, client_name=client_name, client_phone_1=client_phone_1,
+                            client_phone_2=client_phone_2,
+                            client_wechat=client_wechat,
+                            client_address=client_address,
+                            above_floor=above_floor,
+                            under_floor=under_floor,
+                            bedroom_num=bedroom_num,
+                            living_room_num=living_room_num,
+                            rest_room_num=rest_room_num,
+                            client_source=client_source,
+                            price=price,
+                            gift=gift,
+                            work_time=work_time)
+        db.session.add(new_item)
+        db.session.commit()
+
+        return jsonify({
+            "data": new_item.id
+        })
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"msg": str(e)})
